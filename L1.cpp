c331a0366ca904c79468908a03d831468ceb25cf
sonnned <alejo1garciasosa@gmail.com>
2023-06-10 19:30:33 -0500

    FC

---
+++ L1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello, World!" << endl;
+  cout << "Bye, World!" << endl;
+}
+
+/*
+ * Explicación:
+ * cout viene de la librería iostream y nos permite imprimir en pantalla.
+ * << es un operador que nos permite concatenar cadenas de texto. (interceptor operator)
+ * "Hello, World!" es una cadena de texto.
+ * 
+ * Si queremos imprimir varias líneas, al final de cada línea debemos poner << endl que es un salto de línea.
+*/
---
+++ README.md
@@ -0,0 +1,11 @@
+# Compilar y correr .cpp
+Para compilar usamos
+```bash
+g++ -o nombre_que_queremos_dar ./archivo.cpp
+```
+Para correr
+```bash
+./nombre_que_queremos_dar.exe
+```
+
+# Variables y constantes
